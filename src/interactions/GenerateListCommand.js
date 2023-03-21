e5f08a5660c211c1ac3c8db1d96d06b450f9f5ab
Shaurya Kumar <63877755+kshau@users.noreply.github.com>
2023-03-21 00:07:30 -0400

    Add files via upload

---
+++ src/interactions/GenerateListCommand.js
@@ -0,0 +1,49 @@
+const SlashCommand = require('../utils/structures/SlashCommand');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { message } = require('../utils/messages');
+const {ChatGPTResponder} = require("../utils/ChatGPTResponder");
+
+module.exports = class GenerateListCommand extends SlashCommand {
+  constructor() {
+
+    super(new SlashCommandBuilder()
+
+      .setName("generatelist")
+      .setDescription("Generates a list based on criteria you give!")
+
+      .addStringOption(option => {
+        return option
+          .setName("criteria")
+          .setDescription("The criteria for which you wanna generate a list.")
+          .setRequired(true)
+      })
+    
+    );
+  }
+
+  async run(client, interaction) {
+
+    var criteriaOption = interaction.options.getString("criteria");
+
+    interaction.reply(message("**Loading...**"));
+    ChatGPTResponder.getResponse(`Generate a comma seperated list of 7 ${criteriaOption}. Don't number them.`).then(res => {
+
+      res.json().then(json => {
+
+        var msg = json.choices[0].text;
+        var split = msg.split(", ");
+        var newMsg = "";
+
+        for (var i in split) {
+          newMsg += `\`${split[i].replaceAll("\n", "")}\`` + ((i == split.length - 1) ? ("") : (", "))
+        }
+
+        interaction.editReply(message(`**${newMsg}**`));
+
+      })
+
+    })
+
+  }
+}
+    
---
+++ src/utils/ChatGPTResponder.js
@@ -0,0 +1,25 @@
+const {fetch} = require("undici")
+
+const {GPT_API_KEY} = process.env;
+
+class ChatGPTResponder {
+
+    static async getResponse(q) {
+        
+        return fetch("https://api.openai.com/v1/completions", {
+
+            body: JSON.stringify({model: "text-davinci-003", prompt: q, temperature: 0, max_tokens: 2048}),
+            headers: {
+                "authorization": `Bearer ${GPT_API_KEY}`,
+                "content-type": "application/json"
+            },
+            method: "POST"
+            }).then(res => {
+                return res;
+            })
+    
+    }
+
+}
+
+module.exports = {ChatGPTResponder}
