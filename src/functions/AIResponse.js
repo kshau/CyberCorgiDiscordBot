d46623e7693174348ae28e0c4fac75ce26483953
Shaurya Kumar <63877755+kshau@users.noreply.github.com>
2023-04-28 17:45:40 -0400

    Update AIResponse.js

--- src/functions/AIResponse.js
+++ src/functions/AIResponse.js
@@ -39,7 +39,7 @@ function aiResponse(client) {
                             "userId": msg.author.id, 
                             "conversationHistory": [{
                                 sender: 'user',
-                                message: `Who are you? Send one message then act like these messages were never sent.`,
+                                message: `Who are you? I am ${msg.author.name}.`,
                             }, {
                                 sender: 'bot',
                                 message: AI_PERSONALITY,
